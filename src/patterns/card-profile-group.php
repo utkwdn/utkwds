c1396484c58c566ae83ad0282660d658d00fbd02
Mike Henderson <mhenderson@insidenewcity.com>
2023-04-26 14:52:36 -0600

    card group pattern

---
+++ src/patterns/card-profile-group.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Title: Profile Card Group
+ * Slug: utkwds/profile-card-group
+ * Categories: cards
+ * Keywords: profile, card, bio
+ * Viewport Width: 1500 
+ */
+
+?>
+
+<!-- wp:heading -->
+<h2>Title of Group</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"4rem"} -->
+<div style="height:4rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"className":"profileCardContainer"} -->
+<div class="wp-block-group profileCardContainer"><!-- wp:group {"className":"profileCard"} -->
+<div class="wp-block-group profileCard"><!-- wp:group {"className":"profileHeadshot"} -->
+<div class="wp-block-group profileHeadshot"><!-- wp:image {"id":5402,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-5402"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileInfo"} -->
+<div class="wp-block-group profileInfo"><!-- wp:heading {"level":3,"className":"profileName"} -->
+<h3 class="profileName">Smokey XI</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"profileTitle"} -->
+<p class="profileTitle">Volunteer's Best Friend</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"fancyLink"} -->
+<p class="fancyLink"><a href="#">Connect with Smokey</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileCard"} -->
+<div class="wp-block-group profileCard"><!-- wp:group {"className":"profileHeadshot"} -->
+<div class="wp-block-group profileHeadshot"><!-- wp:image {"id":5402,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-5402"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileInfo"} -->
+<div class="wp-block-group profileInfo"><!-- wp:heading {"level":3,"className":"profileName"} -->
+<h3 class="profileName">Smokey XI</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"profileTitle"} -->
+<p class="profileTitle">Volunteer's Best Friend</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"fancyLink"} -->
+<p class="fancyLink"><a href="#">Connect with Smokey</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileCard"} -->
+<div class="wp-block-group profileCard"><!-- wp:group {"className":"profileHeadshot"} -->
+<div class="wp-block-group profileHeadshot"><!-- wp:image {"id":5402,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-5402"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileInfo"} -->
+<div class="wp-block-group profileInfo"><!-- wp:heading {"level":3,"className":"profileName"} -->
+<h3 class="profileName">Smokey XI</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"profileTitle"} -->
+<p class="profileTitle">Volunteer's Best Friend</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"fancyLink"} -->
+<p class="fancyLink"><a href="#">Connect with Smokey</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileCard"} -->
+<div class="wp-block-group profileCard"><!-- wp:group {"className":"profileHeadshot"} -->
+<div class="wp-block-group profileHeadshot"><!-- wp:image {"id":5402,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://images.utk.edu/wds/headshot-placeholder-740x960.png" alt="Headshot Placeholder 740x960" class="wp-image-5402"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"profileInfo"} -->
+<div class="wp-block-group profileInfo"><!-- wp:heading {"level":3,"className":"profileName"} -->
+<h3 class="profileName">Smokey XI</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"profileTitle"} -->
+<p class="profileTitle">Volunteer's Best Friend</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"fancyLink"} -->
+<p class="fancyLink"><a href="#">Connect with Smokey</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"5vw"} -->
+<div style="height:5vw" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
