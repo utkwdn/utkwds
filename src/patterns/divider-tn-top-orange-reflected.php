75845b409a039231bd949fd2b08f1c500a268dd8
Mike Henderson <mhenderson@insidenewcity.com>
2023-04-26 14:52:26 -0600

    divider patterns

---
+++ src/patterns/divider-tn-top-orange-reflected.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Divider TN Top Orange Reflected
+ * Slug: utkwds/divider-tn-top-orange-reflected
+ * Categories: divider
+ * Keywords: orange, divider, top, reflect
+ * Viewport Width: 1500
+ */
+?>
+<!-- wp:group {"align":"full","className":"tnDivider reflect"} -->
+<div class="wp-block-group alignfull tnDivider reflect"><!-- wp:group {"backgroundColor":"orange","className":"tnTop "} -->
+<div class="wp-block-group tnTop has-orange-background-color has-background"></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
