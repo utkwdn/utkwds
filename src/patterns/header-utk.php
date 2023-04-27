8408cefc9f97f3dcd9ceb89a8edc23c81dc6d45e
Mike Henderson <mhenderson@insidenewcity.com>
2023-04-27 15:43:19 -0600

    header footer adjustments

--- src/patterns/header-utk.php
+++ src/patterns/header-utk.php
@@ -3,30 +3,28 @@
  * Title: Header with site title, navigation.
  * Slug: utkwds/header-utk
  * Categories: header
+ * Inserter: false
  * Block Types: core/template-part/header
  */
 ?>
 
+<!-- wp:html -->
 <nav id="universal-header">
-    <div class="container-xxl">
-      <ul class="menu-universal ">
+	<div class="container-xxl">
+		<ul class="menu-universal ">
 <!--         <li class="col-auto list-inline-item me-md-auto"><small><a href="https://www.utk.edu" class="nav-link text-light text-uppercase text-center text-lg-left">www.utk.edu</a></small></li> -->
-        <li class="uni-nav-item"><small><a href="https://www.utk.edu/requestinfo/" class="menu-item text-light text-uppercase">Request Info</a></small></li>
-        <li class="uni-nav-item"><small><a href="https://www.utk.edu/visit/" class="menu-item text-light text-uppercase">Visit</a></small></li>
-        <li class="uni-nav-item"><small><a href="https://www.utk.edu/apply/" class="menu-item text-light text-uppercase">Apply</a></small></li>
-        <li class="uni-nav-item"><small><a href="https://www.utk.edu/give/" class="menu-item text-light text-uppercase">Give</a></small></li>
-        <li class="uni-nav-item"> <button type="button" id="btn-searchopen" class="btn btn-search text-uppercase text-light navbar-toggler col-auto collapsed px-0" data-bs-toggle="modal" data-bs-target="#searchModal" aria-label="Toggle search">
-			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24" id="searchHeader-open">
-			<path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"></path>
-		</svg>
-  <span>Search</span></button></li>
-      </ul>
-
-
-    </div>
-  </nav>
-
-
+			<li class="uni-nav-item"><small><a href="https://www.utk.edu/requestinfo/" class="menu-item text-light text-uppercase">Request Info</a></small></li>
+			<li class="uni-nav-item"><small><a href="https://www.utk.edu/visit/" class="menu-item text-light text-uppercase">Visit</a></small></li>
+			<li class="uni-nav-item"><small><a href="https://www.utk.edu/apply/" class="menu-item text-light text-uppercase">Apply</a></small></li>
+			<li class="uni-nav-item"><small><a href="https://www.utk.edu/give/" class="menu-item text-light text-uppercase">Give</a></small></li>
+			<li class="uni-nav-item"> <button type="button" id="btn-searchopen" class="btn btn-search text-uppercase text-light navbar-toggler col-auto collapsed px-0" data-bs-toggle="modal" data-bs-target="#searchModal" aria-label="Toggle search">
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24" id="searchHeader-open">
+					<path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"></path>
+				</svg>
+			<span>Search</span></button></li>
+		</ul>
+	</div>
+</nav>
 
 <!-- Modal -->
 <div class="modal fade" id="searchModal" tabindex="-1"  aria-hidden="true">
@@ -459,3 +457,4 @@
     </div>
   </div>
 </div>
+<!-- /wp:html -->
