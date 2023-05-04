113485c9cbb4d2f6007cdec2e93e463604713bf3
Jesse Janowiak <wonkeythemonkey@gmail.com>
2023-05-04 13:17:20 -0400

    Add support for archive pages to breadcrumbs

--- plugins/utk-wds-breadcrumbs/src/classes/Breadcrumbs.php
+++ plugins/utk-wds-breadcrumbs/src/classes/Breadcrumbs.php
@@ -11,6 +11,7 @@
 
  class Breadcrumbs {
     protected $post;
+    protected $post_type;
     protected $links;
 
     public function __construct( WP_Post|int $post = 0 ) {
@@ -20,6 +21,10 @@
             $this->post = get_post( $post );
         } else {
             $this->post = get_post();
+        }
+
+        if ($this->post) {
+            $this->post_type = $this->post->post_type;
         }
 
         $this->links = $this->default_breadcrumb_links();
@@ -39,6 +44,15 @@
         );
     }
 
+    protected function add_current_link( array $links ): array {
+        if (is_singular()) {
+            $current_link = Navigation::convert_post_to_link( $this->post, get_the_ID($this->post) );
+            return array_merge( $links, array( $current_link ) );
+        }
+
+        return $links;
+    }
+
     protected function default_breadcrumb_links(): array {
 
         if (! $this->post) {
@@ -47,18 +61,32 @@
         
         $initial_links = static::default_initial_links();
 
-        $current_link = Navigation::convert_post_to_link( $this->post, get_the_ID($this->post) );
-
         $ancestors = get_post_ancestors( $this->post );
+
+        $breadcrumb_links = array();
+
         if ( is_array( $ancestors ) && count( $ancestors ) > 0 ) {
-            $breadcrumb_links = array_map( function ( $ancestor ) {
+            $ancestor_links = array_map( function ( $ancestor ) {
                 return Navigation::convert_post_to_link( $ancestor, get_the_ID($this->post) );
             }, $ancestors );
-        } else {
-            $breadcrumb_links = array();
+            $breadcrumb_links = array_merge( $breadcrumb_links, $ancestor_links );
         }
 
-        return array_merge( $initial_links, $breadcrumb_links, array( $current_link ) );
+        if( ! ($this->post_type === 'page' || $this->post_type === 'post') ) {
+            $post_type_archive_url = get_post_type_archive_link( $this->post_type );
+            if ( $post_type_archive_url ) {
+                $archive_links = array(
+                    array(
+                        'title' => get_post_type_object( $this->post_type )->labels->name,
+                        'url' => $post_type_archive_url,
+                        'isCurrent' => is_post_type_archive($this->post_type) ? true : false,
+                    ),
+                );
+                $breadcrumb_links = array_merge( $breadcrumb_links, $archive_links );
+            }
+        }
+
+        return $this->add_current_link( array_merge( $initial_links, $breadcrumb_links ) );
     }
 
  }
