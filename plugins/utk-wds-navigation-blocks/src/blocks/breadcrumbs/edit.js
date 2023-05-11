e337ddff72c3706211e18beef24b0169a6af6000
Jesse Janowiak <wonkeythemonkey@gmail.com>
2023-05-11 16:49:46 -0400

    Added more useful edit-side message

--- plugins/utk-wds-navigation-blocks/src/blocks/breadcrumbs/edit.js
+++ plugins/utk-wds-navigation-blocks/src/blocks/breadcrumbs/edit.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from "@wordpress/i18n";
+import { __ } from '@wordpress/i18n';
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -11,7 +11,7 @@ import { __ } from "@wordpress/i18n";
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from "@wordpress/block-editor";
+import { useBlockProps } from '@wordpress/block-editor';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -19,7 +19,7 @@ import { useBlockProps } from "@wordpress/block-editor";
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import "./editor.scss";
+import './editor.scss';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -32,7 +32,10 @@ import "./editor.scss";
 export default function Edit() {
 	return (
 		<p {...useBlockProps()}>
-			{__("Breadcrumbs – hello from the editor!", "utk-wds")}
+			{__(
+				'Breadcrumbs blocks are not editable using the Block Editor interface. Any modifications should be made directly to the Block markup.',
+				'utk-wds'
+			)}
 		</p>
 	);
 }
