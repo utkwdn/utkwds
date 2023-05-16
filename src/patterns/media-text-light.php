0a9e656e57d3d7e47fa73f1c15c4a947c831b4a2
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-16 09:54:16 -0600

    wip media text pattern

---
+++ src/patterns/media-text-light.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Media Text Light
+ * Slug: utkwds/media-text-light
+ * Description: 
+ * Categories: 
+ * Keywords: media, 50/50, light
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:media-text {"align":"full","backgroundColor":"gray2"} -->
+<div class="wp-block-media-text alignfull is-stacked-on-mobile has-gray-2-background-color has-background"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://utk.edu">Learn More</a></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
---
+++ src/patterns/media-text-smokey.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Media Text Smokey
+ * Slug: utkwds/media-text-smokey
+ * Description: 
+ * Categories: 
+ * Keywords: media, 50/50, smokey
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:media-text {"align":"full","backgroundColor":"smokey","textColor":"white"} -->
+<div class="wp-block-media-text alignfull is-stacked-on-mobile has-white-color has-smokey-background-color has-text-color has-background"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading {"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}}} -->
+<p class="has-link-color"><a href="https://utk.edu">Learn More</a></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
