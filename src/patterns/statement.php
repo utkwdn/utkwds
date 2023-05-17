2834618317c394b0f12a505f76f6ebc50c38a0db
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-17 12:48:21 -0600

    statement first pass

---
+++ src/patterns/statement.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Statement
+ * Slug: utkwds/statement
+ * Description: 
+ * Categories: 
+ * Keywords: statement, cover
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:cover {"url":"https://images.utk.edu/wds/gallery-2up-placeholder-large.png","dimRatio":50,"contentPosition":"bottom right","align":"full","className":"utkwds-statement"} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-right utkwds-statement"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="https://images.utk.edu/wds/gallery-2up-placeholder-large.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"textColor":"white","fontSize":"small"} -->
+<p class="has-text-align-center has-white-color has-text-color has-small-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">Our programs emphasize learning through hands-on experience, and we stay up to date with industry trends to prepare the next generation of <strong>communication and information leaders</strong>.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
