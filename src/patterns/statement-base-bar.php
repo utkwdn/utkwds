04e768c4516227aaa5d37a8400d4d0b487f5dc11
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-18 09:20:40 -0600

    move base bar to pattern

--- src/patterns/statement-base-bar.php
+++ src/patterns/statement-base-bar.php
@@ -19,7 +19,7 @@
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:cover -->
 
-<!-- wp:group {"templateLock": "contentOnly","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"orange","className":"utkwds-dots-base-bar","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull utkwds-dots-base-bar has-orange-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"></div>
-<!-- /wp:group --></div>
+<!-- wp:pattern {"slug":"utkwds/base-bar"} /-->
+
+</div>
 <!-- /wp:group -->
