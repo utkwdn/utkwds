1a78e36f0a620f608d6b708cd1c61c3cbe31631d
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-26 15:22:57 -0600

    media text fancy links

--- src/patterns/media-text-white.php
+++ src/patterns/media-text-white.php
@@ -21,7 +21,7 @@
 <p></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"className":"utkwds-fancy-link"} -->
-<p class="utkwds-fancy-link"><a href="mailto:email@utk.edu">email@utk.edu</a></p>
+<!-- wp:paragraph {"className":"is-style-utwds-fancy-link"} -->
+<p class="is-style-utwds-fancy-link"><a href="https://www.utk.edu/">Fancy Link</a></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:media-text -->
