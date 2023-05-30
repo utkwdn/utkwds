5192361738a35699c98e5a5372cc2df22abe2fc4
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-30 08:19:19 -0600

    3up cards

---
+++ src/patterns/card-subject-group-3up-light.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Title: Card Subject Group 3up Light
+ * Slug: utkwds/card-subject-group-3up-light
+ * Description:
+ * Categories: card
+ * Keywords: profile, card, bio, light
+ * Viewport Width: 1500 
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+
+?>
+
+<!-- wp:columns {"className":"utkwds-card-contact-group"} -->
+<div class="wp-block-columns utkwds-card-contact-group"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"templateLock": "contentOnly","style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-card-subject","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group utkwds-card-subject has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<h3 class="wp-block-heading has-medium-font-size"></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column">
+  
+<!-- wp:group {"templateLock": "contentOnly","style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-card-subject","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group utkwds-card-subject has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<h3 class="wp-block-heading has-medium-font-size"></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column">
+  
+<!-- wp:group {"templateLock": "contentOnly","style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-card-subject","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group utkwds-card-subject has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<h3 class="wp-block-heading has-medium-font-size"></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
