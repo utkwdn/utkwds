4d45d66ebf45019a27eb29afc158be2209fd12be
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-30 14:47:14 -0600

    fancy link group

---
+++ src/patterns/fancy-link-group-white.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Fancy Link Group White
+ * Slug: utkwds/fancy-link-group-white
+ * Description:
+ * Categories: content
+ * Keywords: fancy link, white
+ * Viewport Width: 460 
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:list {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"className":"utkwds-fancy-link-group is-style-no-disc"} -->
+<ul class="utkwds-fancy-link-group is-style-no-disc" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:list-item -->
+<li><a href="https://www.utk.edu/">Fancy Link</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://www.utk.edu/">Fancy Link</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://www.utk.edu/">Fancy Link</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
