7c9549a45cf779a20a0f74912762c1432ab53a9d
Mike Henderson <mhenderson@insidenewcity.com>
2023-05-31 14:38:38 -0600

    image frame block style

--- src/inc/functions/block-styles.php
+++ src/inc/functions/block-styles.php
@@ -1,9 +1,61 @@
 <?php
 
-register_block_style(
-  'core/separator',
-    array(
-      'name' => 'utkwds-orange-separator',
-      'label' => __( 'Orange', 'utkwds' ),
-    )
-);
+/**
+ * Register block styles.
+ *
+ * @since 0.9.2
+ */
+function utkwds_register_block_styles() {
+
+	$block_styles = array(
+		'core/columns' => array(
+			'columns-reverse' => __( 'Reverse', 'utkwds' ),
+		),
+    'core/image' => array(
+      'utkwds-left-frame' => __( 'Left Frame', 'utkwds' ),
+      'utkwds-right-frame' => __( 'Right Frame', 'utkwds' ),
+    ),
+		'core/list' => array(
+			'no-disc' => __( 'No Disc', 'utkwds' ),
+		),
+		'core/navigation-link' => array(
+			'outline' => __( 'Outline', 'utkwds' ),
+		),
+    'core/separator' => array(
+      'utkwds-orange-separator' => __( 'Orange', 'utkwds' ),
+    ),
+		'core/social-links' => array(
+			'outline' => __( 'Outline', 'utkwds' ),
+		),
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', 'utkwds_register_block_styles' );
+
+
+/**
+ * Unregister block styles.
+ */
+
+function utkwds_unregister_block_styles() {
+  wp_enqueue_script(
+    'utkwds-unregister',
+    get_stylesheet_directory_uri() . '/js/unregister.js',
+    array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+    UTKDS_VERSION,
+    true
+  );
+}
+
+add_action( 'enqueue_block_editor_assets', 'utkwds_unregister_block_styles' );
---
+++ src/js/unregister.js
@@ -0,0 +1,5 @@
+// Unregister block styles
+
+wp.domReady(() => {
+  wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+});
