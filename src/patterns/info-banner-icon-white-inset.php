05ba0f8e209c3b75493f8a08a24c046900dbd979
Mike Henderson <mhenderson@insidenewcity.com>
2023-06-01 15:55:23 -0600

    info banner patterns

---
+++ src/patterns/info-banner-icon-white-inset.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Info Banner Icon White Inset
+ * Slug: utkwds/info-banner-icon-white-inset
+ * Description: 
+ * Categories: content
+ * Keywords: info, banner, icon, white
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"align":"full","className":"utkwds-info-banner-icon","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull utkwds-info-banner-icon"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"white"} -->
+<div class="wp-block-columns has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:column {"verticalAlignment":"top","width":"30%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:30%"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none","className":"size-full"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/assets/images/icon-globe.png" alt="" /></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"70%"} -->
+<div class="wp-block-column" style="flex-basis:70%"><!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-utwds-fancy-link"} -->
+<p class="is-style-utwds-fancy-link"><a href="https://www.utk.edu/">Fancy Link</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ src/patterns/info-banner-image-light.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Info Banner Image Light
+ * Slug: utkwds/info-banner-image-light
+ * Description: 
+ * Categories: content
+ * Keywords: info, banner, image, light
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-info-banner-image","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull utkwds-info-banner-image has-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:columns {"verticalAlignment":null} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"30%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:30%"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"70%"} -->
+<div class="wp-block-column" style="flex-basis:70%"><!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-utwds-fancy-link"} -->
+<p class="is-style-utwds-fancy-link"><a href="https://www.utk.edu/">Fancy Link</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
