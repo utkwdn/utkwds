90c07620ec0c462d0724df1fb81ab86e32e0eda4
Mike Henderson <mhenderson@insidenewcity.com>
2023-06-06 12:03:54 -0600

    header variations

--- src/patterns/header-fancy-link-light-image-left.php
+++ src/patterns/header-fancy-link-light-image-left.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Header Fancy Link Light
- * Slug: utkwds/header-fancy-link-light
+ * Title: Header Fancy Link Light Image Left
+ * Slug: utkwds/header-fancy-link-light-image-left
  * Description:
  * Categories: hero
  * Keywords: media, header, link list, fancy link, light
@@ -16,6 +16,12 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-header-fancy-link","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull utkwds-header-fancy-link has-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"linkDestination":"none","className":"is-style-utkwds-left-frame"} -->
+<figure class="wp-block-image is-style-utkwds-left-frame"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading -->
 <h2 class="wp-block-heading"></h2>
 <!-- /wp:heading -->
@@ -24,15 +30,19 @@
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:pattern {"slug":"utkwds/fancy-link-group-white"} /-->
+<!-- wp:list {"className":"utkwds-fancy-link-group is-style-no-disc"} -->
+<ul class="utkwds-fancy-link-group is-style-no-disc"><!-- wp:list-item -->
+<li><a href="https://www.utk.edu/">Fancy Link</a></li>
+<!-- /wp:list-item -->
 
-</div>
-<!-- /wp:column -->
+<!-- wp:list-item -->
+<li><a href="https://www.utk.edu/">Fancy Link</a></li>
+<!-- /wp:list-item -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"linkDestination":"none","className":"is-style-utkwds-left-frame"} -->
-<figure class="wp-block-image is-style-utkwds-left-frame"><img  alt="" /></figure>
-<!-- /wp:image --></div>
+<!-- wp:list-item -->
+<li><a href="https://www.utk.edu/">Fancy Link</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
