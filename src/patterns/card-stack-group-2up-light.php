6d8cd34d071cede0a94c7659242a63e917549c34
Mike Henderson <mhenderson@insidenewcity.com>
2023-06-09 12:51:04 -0600

    card stack styling

--- src/patterns/card-stack-group-2up-light.php
+++ src/patterns/card-stack-group-2up-light.php
@@ -19,16 +19,19 @@
 <!-- /wp:heading -->
 
 <!-- wp:columns -->
-<div class="wp-block-columns">
-<!-- wp:column {"templateLock": "contentOnly","style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","layout":{"type":"default"}} -->
-<div class="wp-block-column has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-card-stack","layout":{"type":"default"}} -->
+<div class="wp-block-column utkwds-card-stack has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full"><img alt="" /></figure>
 <!-- /wp:image -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|60","bottom":"0","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"></h3>
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<h3 class="wp-block-heading has-medium-font-size"></h3>
 <!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra</p>
+<!-- /wp:paragraph -->
 
 <!-- wp:list {"className":"is-style-no-disc"} -->
 <ul class="is-style-no-disc"><!-- wp:list-item -->
@@ -46,15 +49,19 @@
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"templateLock": "contentOnly","style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","layout":{"type":"default"}} -->
-<div class="wp-block-column has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|60","padding":{"bottom":"var:preset|spacing|60"}}},"backgroundColor":"light","className":"utkwds-card-stack","layout":{"type":"default"}} -->
+<div class="wp-block-column utkwds-card-stack has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full"><img alt="" /></figure>
 <!-- /wp:image -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|60","bottom":"0","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"></h3>
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"level":3,"fontSize":"medium"} -->
+<h3 class="wp-block-heading has-medium-font-size"></h3>
 <!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra</p>
+<!-- /wp:paragraph -->
 
 <!-- wp:list {"className":"is-style-no-disc"} -->
 <ul class="is-style-no-disc"><!-- wp:list-item -->
