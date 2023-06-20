906f246a2b4b5f2912db7535635f2ac72d9bca03
Jesse Janowiak <wonkeythemonkey@gmail.com>
2023-06-20 11:20:24 -0400

    Do not track deploy to local script

--- plugins/utk-wds-navigation-blocks/deploy-to-local.sh
+++
@@ -1,7 +0,0 @@
-#/bin/zsh
-
-npm run build
-npm run plugin-zip
-rm -rf ~/Local\ Sites/wds/app/public/wp-content/plugins/utk-wds-navigation-blocks
-unzip utk-wds-navigation-blocks.zip -d ~/Local\ Sites/wds/app/public/wp-content/plugins/utk-wds-navigation-blocks/
-rm utk-wds-navigation-blocks.zip
