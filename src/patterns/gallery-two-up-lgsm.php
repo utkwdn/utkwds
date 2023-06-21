f64954c819cde1f11a20b02c58f0d9af6961215a
Mike Henderson <mhenderson@insidenewcity.com>
2023-06-21 08:43:50 -0600

    gallery patterns, dots, style

---
+++ src/patterns/gallery-two-up-lgsm.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Gallery Two Up Large Small
+ * Slug: utkwds/gallery-two-up-lgsm
+ * Categories: gallery
+ * Keywords: gallery, image, orange
+ * Inserter: false
+ * Viewport Width: 1500
+ */
+
+?>
+
+<!-- wp:columns {"className":"utkwds-gallery-two-up-lgsm"} -->
+<div class="wp-block-columns utkwds-gallery-two-up-lgsm">
+
+<!-- wp:column {"width":"57%"} -->
+<div class="wp-block-column" style="flex-basis:57%"><!-- wp:image {"align":"left","id":5004,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image alignleft size-full"><img src="https://images.utk.edu/wds/gallery-2up-placeholder-large.png" alt="Large Image Placeholder 1108 x 736" class="wp-image-5004"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"39%"} -->
+<div class="wp-block-column" style="flex-basis:39%"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"right","id":5016,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image alignright size-full"><img src="https://images.utk.edu/wds/gallery-2up-placeholder-small.png" alt="Small Image Placeholder 745 x 527" class="wp-image-5016"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
