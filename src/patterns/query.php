cb2d5342fb950d4d5c49bd6e9ea8eebe548c494b
Jesse Janowiak <wonkeythemonkey@gmail.com>
2023-06-23 16:13:48 -0400

    Trying to add a tab block (broken)

--- src/patterns/query.php
+++ src/patterns/query.php
@@ -7,6 +7,11 @@
 ?>
 <!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"type":"constrained"}} -->
 <div class="wp-block-query">
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p>No results found for your search.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results -->
 <!-- wp:post-template -->
 <!-- wp:group {"style":{"spacing":{"blockGap":"10px"}}} -->
 <div class="wp-block-group">
