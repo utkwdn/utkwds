77704372ebe427efa8b27a54de6b1892ec95f9d0
Mike Henderson <mhenderson@insidenewcity.com>
2023-06-23 08:41:00 -0600

    content fancy link fix

--- src/patterns/content-fancy-links.php
+++ src/patterns/content-fancy-links.php
@@ -12,19 +12,19 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"backgroundColor":"smokey","textColor":"white","className":"utkwds-content-quick-links","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull utkwds-content-quick-links has-white-color has-smokey-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"align":"wide","textColor":"white"} -->
-<h2 class="wp-block-heading alignwide has-white-color has-text-color"></h2>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"backgroundColor":"smokey","className":"utkwds-content-quick-links","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull utkwds-content-quick-links has-smokey-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h2 class="wp-block-heading alignwide has-white-color has-text-color has-link-color"></h2>
 <!-- /wp:heading -->
 
 <!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"60%"} -->
-<div class="wp-block-column" style="flex-basis:60%"><!-- wp:paragraph -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"60%","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-link-color" style="flex-basis:60%"><!-- wp:paragraph -->
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:list {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"smokey","textColor":"white","className":"utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color"} -->
-<ul class="utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color has-white-color has-smokey-background-color has-text-color has-background"><!-- wp:list-item -->
+<!-- wp:list {"className":"utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color"} -->
+<ul class="utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color"><!-- wp:list-item -->
 <li><a href="https://www.utk.edu/">Fancy Link</a></li>
 <!-- /wp:list-item -->
 
@@ -35,13 +35,12 @@
 <!-- wp:list-item -->
 <li><a href="https://www.utk.edu/">Fancy Link</a></li>
 <!-- /wp:list-item --></ul>
-<!-- /wp:list -->
-</div>
+<!-- /wp:list --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"40%"} -->
-<div class="wp-block-column" style="flex-basis:40%"><!-- wp:paragraph {"textColor":"white"} -->
-<p class="has-white-color has-text-color"><strong>Questions? Here to Help!</strong></p>
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color"><strong>Questions? Here to Help!</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:pattern {"slug":"utkwds/card-contact-white"} /-->
@@ -50,3 +49,4 @@
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
+
