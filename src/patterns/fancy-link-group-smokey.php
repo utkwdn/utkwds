daaca33218e31d9d4c57a736387d6b07b08751de
Mike Henderson <mhenderson@insidenewcity.com>
2023-06-26 15:34:21 -0600

    fancy link padding fix

--- src/patterns/fancy-link-group-smokey.php
+++ src/patterns/fancy-link-group-smokey.php
@@ -13,8 +13,8 @@
 
 ?>
 
-<!-- wp:list {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"smokey","textColor":"white","className":"utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color"} -->
-<ul class="utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color has-white-color has-smokey-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:list-item -->
+<!-- wp:list {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"smokey","textColor":"white","className":"utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color"} -->
+<ul class="utkwds-fancy-link-group is-style-no-disc has-light-background has-link-color has-white-color has-smokey-background-color has-text-color has-background"><!-- wp:list-item -->
 <li><a href="https://www.utk.edu/">Fancy Link</a></li>
 <!-- /wp:list-item -->
 
