2111a3008826193dcb167a8f4064c2645bdc2aae
Jesse Janowiak <wonkeythemonkey@gmail.com>
2023-06-28 10:29:01 -0400

    Insert curson on open, close with Esc key

--- src/js/search-field-focus.js
+++
@@ -1,6 +0,0 @@
-/**
- * After the bootstrap collapse animation is complete on the search field, focus on the search field.
- */
-$(document).on('shown.bs.collapse', '.search-form', function () {
-  $('.search-form input[type="search"]').focus();
-});
