eafdc00d01fb7522412974ec13f8d30a9ad41f4f
Jesse Janowiak <wonkeythemonkey@gmail.com>
2023-06-30 09:57:10 -0400

    No breadcrumb header variants

---
+++ src/patterns/header-utk-no-title-no-breadcrumbs.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Header with NO site title OR breadcrumbs
+ * Slug: utkwds/header-utk-no-title-no-breadcrumbs
+ * Categories: header
+ * Inserter: true
+ * Block Types: core/template-part/header
+ */
+
+ namespace UTK\WebDesignSystem;
+?>
+<!-- wp:utk-wds/site-header { "mainMenuName": "Main Nav Menu", "utilityMenuName": "Utility Nav Menu" } /-->
+
+<!-- wp:utk-wds/nav-menu { "menuName": "Main Nav Menu", "depth": "1", "id": "main-nav--large", "className": "main-nav--large", "interactive": "dropdown", "duplicate_top_links": true } /-->
