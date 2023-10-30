20115722103c0bd96f052a3309bf7b4aec85be6b
Mike Henderson <mhenderson@insidenewcity.com>
2023-10-30 16:22:57 -0600

    new style adjust

--- src/patterns/query-archive.php
+++ src/patterns/query-archive.php
@@ -8,29 +8,29 @@
 
 <!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":10},"className":"utkwds-query-archive","layout":{"type":"constrained"}} -->
 <div class="wp-block-query utkwds-query-archive"><!-- wp:post-template {"layout":{"type":"grid","columnCount":2}} -->
-<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|small"}}},"backgroundColor":"light","className":"utkwds-stack","layout":{"type":"constrained"}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|small"}},"backgroundColor":"light","className":"utkwds-stack","layout":{"type":"constrained"}} -->
 <div class="wp-block-group utkwds-stack has-light-background-color has-background" style="padding-bottom:var(--wp--preset--spacing--small)"><!-- wp:post-featured-image {"isLink":true} /-->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|60","bottom":"0","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small","padding":{"right":"var:preset|spacing|60","bottom":"0","left":"var:preset|spacing|60"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:post-title {"isLink":true,"fontSize":"medium"} /-->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"5px","margin":{"bottom":"30px"}},"typography":{"fontSize":"18px"}},"className":"post-meta","layout":{"type":"flex"}} -->
-<div class="wp-block-group post-meta" style="margin-bottom:30px;font-size:18px">
+<!-- wp:group {"style":{"spacing":{"blockGap":"5px"},"typography":{"fontSize":"18px"}},"className":"post-meta","layout":{"type":"flex"}} -->
+<div class="wp-block-group post-meta" style="font-size:18px">
 
 <?php if ( get_theme_mod( 'show_date' ) == 'show' ) : ?>
-  <!-- wp:post-date /-->
-  <?php endif; ?>
+<!-- wp:post-date {"textColor":"smokeyX"} /-->
+<?php endif; ?>
 
-  <?php if ( get_theme_mod( 'show_author' ) == 'show' ) : ?>
-  <!-- wp:post-author-name {"isLink":true} /-->
-  <?php endif; ?>
-
+<?php if ( get_theme_mod( 'show_author' ) == 'show' ) : ?>
+<!-- wp:post-author-name {"isLink":true} /-->
+<?php endif; ?>
 
 </div>
 <!-- /wp:group -->
 
 <!-- wp:post-excerpt /-->
-<!-- wp:post-terms {"term":"category","separator":""} /--></div>
+
+<!-- wp:post-terms {"term":"category","separator":"","style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"fontSize":"x-small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 <!-- /wp:post-template -->
