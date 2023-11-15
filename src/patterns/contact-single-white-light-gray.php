720fe980d60e0372f0af75d1710655646f13731f
Mike Henderson <mhenderson@insidenewcity.com>
2023-11-15 13:55:52 -0700

    contact card adjustments

---
+++ src/patterns/contact-single-white-light-gray.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Contact single white
+ * Slug: utkwds/contact-single-white-light-gray
+ * Description:
+ * Categories: contact-cards
+ * Keywords: profile, card, bio, white
+ * Viewport Width: 500 
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"align":"full","backgroundColor":"light","layout":{"type":"constrained","wideSize":""},"metadata":{"name":"Contact single white"}} -->
+<div class="wp-block-group alignfull has-light-background-color has-background">
+
+<!-- wp:pattern {"slug":"utkwds/contact-single-white"} /-->
+
+</div>
+<!-- /wp:group -->
