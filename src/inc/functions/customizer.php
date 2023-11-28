55f82cfec40187d3441609c3499074743b8a0552
Mike Henderson <mhenderson@insidenewcity.com>
2023-11-28 15:34:53 -0700

    add footer customizer settings

--- src/inc/functions/customizer.php
+++ src/inc/functions/customizer.php
@@ -7,10 +7,9 @@ function utkwds_customize_register ( $wp_customize ) {
     'description' => __('Settings for the Post Article page template and archive.', 'utkwds')
   ));
 
-
   $wp_customize->add_setting( 'show_author', array(
     'default' => 'show',
-    'transport' => 'refresh'
+    'sanitize_callback' => 'utkwds_sanitize_radio'
   ));
 
   $wp_customize->add_control( 'show_author', array(
@@ -27,7 +26,7 @@ function utkwds_customize_register ( $wp_customize ) {
 
   $wp_customize->add_setting( 'show_date', array(
     'default' => 'show',
-    'transport' => 'refresh'
+    'sanitize_callback' => 'utkwds_sanitize_radio'
   ));
 
   $wp_customize->add_control( 'show_date', array(
@@ -42,6 +41,180 @@ function utkwds_customize_register ( $wp_customize ) {
     )
   ));
 
+  $wp_customize->add_section( 'contact_info_settings', array(
+    'title' => __('Contact Info Settings', 'utkwds'),
+    'description' => __('Settings for the Contact Info in the Footer.', 'utkwds')
+  ));
+  
+  $wp_customize->add_setting( 'address', array(
+    'default' => '',
+    'sanitize_callback' => 'wp_kses_post',
+  ));
+
+  $wp_customize->add_control( 'address', array(
+    'label' => __('Address', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'textarea',
+  ));
+
+  $wp_customize->add_setting( 'email_label', array(
+    'default' => 'Email',
+    'sanitize_callback' => 'wp_filter_nohtml_kses',
+  ));
+
+  $wp_customize->add_control( 'email_label', array(
+    'label' => __('Email Label', 'utkwds'),
+    'default' => 'Email',
+    'section' => 'contact_info_settings',
+    'type' => 'text',
+  ));
+
+  $wp_customize->add_setting( 'email_address', array(
+    'default' => '',
+    'sanitize_callback' => 'sanitize_email',
+  ));
+
+  $wp_customize->add_control( 'email_address', array(
+    'label' => __('Email Address', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'email',
+  ));
+
+  $wp_customize->add_setting( 'phone_label_1', array(
+    'default' => '',
+    'sanitize_callback' => 'wp_filter_nohtml_kses',
+  ));
+
+  $wp_customize->add_control( 'phone_label_1', array(
+    'label' => __('Phone Label 1', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'text',
+  ));
+
+  $wp_customize->add_setting( 'phone_number_1', array(
+    'default' => '',
+    'sanitize_callback' => 'sanitize_text_field',
+  ));
+  
+  $wp_customize->add_control( 'phone_number_1', array(
+    'label' => __('Phone Number 1', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'tel',
+  ));
+
+  $wp_customize->add_setting( 'phone_label_2', array(
+    'default' => '',
+    'sanitize_callback' => 'wp_filter_nohtml_kses',
+  ));
+
+  $wp_customize->add_control( 'phone_label_2', array(
+    'label' => __('Phone Label 2', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'text',
+  ));
+
+  $wp_customize->add_setting( 'phone_number_2', array(
+    'default' => '',
+    'sanitize_callback' => 'sanitize_text_field',
+  ));
+  
+  $wp_customize->add_control( 'phone_number_2', array(
+    'label' => __('Phone Number 2', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'tel',
+  ));
+
+  $wp_customize->add_setting( 'phone_label_3', array(
+    'default' => '',
+    'sanitize_callback' => 'wp_filter_nohtml_kses',
+  ));
+
+  $wp_customize->add_control( 'phone_label_3', array(
+    'label' => __('Phone Label 3', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'text',
+  ));
+
+  $wp_customize->add_setting( 'phone_number_3', array(
+    'default' => '',
+    'sanitize_callback' => 'sanitize_text_field',
+  ));
+  
+  $wp_customize->add_control( 'phone_number_3', array(
+    'label' => __('Phone Number 3', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'tel',
+  ));
+
+  $wp_customize->add_setting( 'social_url_x', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control( 'social_url_x', array(
+    'label' => __('Twitter/X URL', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'url',
+  ));
+
+  $wp_customize->add_setting( 'social_url_facebook', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control( 'social_url_facebook', array(
+    'label' => __('Facebook URL', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'url',
+  ));
+
+  $wp_customize->add_setting( 'social_url_youtube', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control( 'social_url_youtube', array(
+    'label' => __('YouTube URL', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'url',
+  ));
+
+  $wp_customize->add_setting( 'social_url_instagram', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control( 'social_url_instagram', array(
+    'label' => __('Instagram URL', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'url',
+  ));
+  
+  $wp_customize->add_setting( 'social_url_linkedin', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+
+  $wp_customize->add_control( 'social_url_linkedin', array(
+    'label' => __('LinkedIn URL', 'utkwds'),
+    'section' => 'contact_info_settings',
+    'type' => 'url',
+  ));
+
 }
 
 add_action( 'customize_register', 'utkwds_customize_register' );
+
+//radio box sanitization function
+function utkwds_sanitize_radio( $input, $setting ){
+          
+  //input must be a slug: lowercase alphanumeric characters, dashes and underscores are allowed only
+  $input = sanitize_key($input);
+
+  //get the list of possible radio box options 
+  $choices = $setting->manager->get_control( $setting->id )->choices;
+                    
+  //return input if valid or return default option
+  return ( array_key_exists( $input, $choices ) ? $input : $setting->default );                
+    
+}
