53228f75187e404b9aeb4a3d628524732d34673e
Mike Henderson <mhenderson@insidenewcity.com>
2023-11-29 15:50:57 -0700

    page templates

---
+++ src/patterns/page-directory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Directory Page
+ * Slug: utkwds/page-directory
+ * Categories: page-templates
+ */
+?>
+
+<!-- wp:pattern {"slug":"utkwds/contact-2up-light-gray"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/text-contact-smokey"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/media-text-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/accordion-light-gray"} /-->
---
+++ src/patterns/page-subject-focused.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Subject Focused Page
+ * Slug: utkwds/page-subject-focused
+ * Categories: page-templates
+ */
+?>
+
+<!-- wp:pattern {"slug":"utkwds/media-text-cta-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/text-contact-smokey"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/horizontal-single-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/contact-2up-light-gray"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/subject-3up-light-gray"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/fancy-link-group-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/quote-media-orange-light-gray"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/accordion-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/callout-orange"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/text-media-bg-texture-b"} /-->
