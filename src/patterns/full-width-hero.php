4e47f70659099aec5b14d2ac53d75ad12e98ff57
Mike Henderson <mhenderson@insidenewcity.com>
2024-04-12 14:35:27 -0600

    full width hero pattern and styles

---
+++ src/patterns/full-width-hero.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: Full-Width Hero
+ * Slug: utkwds/full-width-hero
+ * Description: 
+ * Categories: hero
+ * Keywords: hero, cover
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Full-Width Hero"},"align":"full","className":"utkwds-full-width-hero","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull utkwds-full-width-hero"><!-- wp:cover {"url":"http://localhost:8888/wp-content/uploads/2024/04/8ee01028ea088c0b1fbc40463e51462f.jpeg","id":465,"dimRatio":0,"isUserOverlayColor":true,"minHeight":700,"contentPosition":"bottom center","align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-center" style="min-height:700px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-465" alt="" src="http://localhost:8888/wp-content/uploads/2024/04/8ee01028ea088c0b1fbc40463e51462f.jpeg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|small"}},"backgroundColor":"white","textColor":"smokey","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-smokey-color has-white-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"metadata":{"name":"Superhead"},"style":{"typography":{"textTransform":"uppercase","fontStyle":"italic","fontWeight":"400","lineHeight":"0.9"}},"className":"utkwds-superhead","fontSize":"large","fontFamily":"condensed"} -->
+<p class="utkwds-superhead has-condensed-font-family has-large-font-size" style="font-style:italic;font-weight:400;line-height:0.9;text-transform:uppercase">Optional Superhead</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title {"level":1} /-->
+
+<!-- wp:paragraph -->
+<p><strong>Optional Subhead</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Quisque aliquet, massa eget ornare vehicula, urna lectus molestie lacus, in luctus magna arcu et lacus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"className":"is-style-utkwds-fancy-link"} -->
+<p class="is-style-utkwds-fancy-link"><a href="https://www.utk.edu/">Fancy Link</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:pattern {"slug":"utkwds/orange-bar-texture"} /-->
+
+</div>
+<!-- /wp:group -->
