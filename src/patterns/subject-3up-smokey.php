e42a73cef9d2f1ded7d2369953586cf5835c6f07
Mike Henderson <mhenderson@insidenewcity.com>
2024-04-15 11:05:32 -0400

    subject 3up names

--- src/patterns/subject-3up-smokey.php
+++ src/patterns/subject-3up-smokey.php
@@ -14,7 +14,7 @@
 
 ?>
 
-<!-- wp:columns {"className":"utkwds-subject-3up"} -->
+<!-- wp:columns {"metadata":{"name":"Subject 3up Smokey"},"className":"utkwds-subject-3up"} -->
 <div class="wp-block-columns utkwds-subject-3up">
 <!-- wp:column {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"smokey","textColor":"white"} -->
 <div class="wp-block-column has-white-color has-smokey-background-color has-text-color has-background has-link-color">
