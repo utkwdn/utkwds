e0e3517f373c4e0a66e9090668282fdc4e775407
Mike Henderson <mhenderson@insidenewcity.com>
2024-05-10 12:03:43 -0600

    utility menu settings, build menu

--- src/inc/functions/inc-menu.php
+++ src/inc/functions/inc-menu.php
@@ -4,8 +4,7 @@ function utkwds_register_menus() {
 
   register_nav_menus(
     array(
-      'primary' => __( 'Main Nav Menu', 'utkwds' ),
-      'utility' => __( 'Utility Nav Menu', 'utkwds' ),
+      'main' => __( 'Main Nav Menu', 'utkwds' ),
       'footer' => __( 'Footer Links Menu', 'utkwds' ),
     )
   );
@@ -14,8 +13,57 @@ function utkwds_register_menus() {
 
 add_action( 'after_setup_theme', 'utkwds_register_menus' );
 
+function utkwds_build_utility_nav_menu() {
+
+  $utility_menu_name = "Utility Nav Menu";
+  $utility_menu_exists = wp_get_nav_menu_object( $utility_menu_name );
+
+  $utility_items = ['Request Info', 'Visit', 'Apply', 'Give'];
+
+  if ( is_object($utility_menu_exists) ) {
+    $utility_menu_id = $utility_menu_exists->term_id;
+  } 
+
+  if ( !$utility_menu_exists ) {
+    $utility_menu_id = wp_create_nav_menu( $utility_menu_name );
+  }
+
+  foreach( $utility_items as $item ) {
+
+    $args = array( 'title' => $item );
+
+    $current_menu_item = wp_get_nav_menu_items($utility_menu_name, $args );
+    $current_menu_item_id = 0;
+
+    if (empty($current_menu_item) || isset($current_menu_item[0]->ID)) {
+      $current_menu_item_id = $current_menu_item[0]->ID;
+    }
+
+    $menu_url = get_theme_mod( 'utility_menu_' . strtolower(str_replace('', '_', $item)));
+
+    if ( empty($menu_url) || filter_var($menu_url, FILTER_VALIDATE_URL) === false ) {
+      $menu_url = 'https://www.utk.edu/' . strtolower(str_replace(' ', '', $item));
+    }
+
+    wp_update_nav_menu_item( $utility_menu_id, $current_menu_item_id, array(
+        'menu-item-title' =>  __( $item, 'utkwds' ),
+        'menu-item-url' => $menu_url, 
+        'menu-item-status' => 'publish',
+        'menu-item-type' => 'custom',
+    ) );
+
+  }
+
+  //hide utility menu with css???
+
+}
+
+add_action( 'after_setup_theme', 'utkwds_build_utility_nav_menu' );
+add_action( 'wp_update_nav_menu', 'utkwds_build_utility_nav_menu' );
+add_action( 'customize_save_after', 'utkwds_build_utility_nav_menu' );
+
 function utkwds_register_naviation_limit_script( $hook ){
-	
+            
 	wp_register_script( 'utkwds-navigation-limit-depth', false );
 	wp_add_inline_script(
 		'utkwds-navigation-limit-depth',
