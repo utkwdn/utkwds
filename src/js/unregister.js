c242f971a81606f278ca0fb2c32d5839cd0c4b98
Mike Henderson <mhenderson@insidenewcity.com>
2024-06-14 14:26:24 -0600

    social icons

--- src/js/unregister.js
+++ src/js/unregister.js
@@ -6,4 +6,7 @@ wp.domReady(() => {
   wp.blocks.unregisterBlockStyle('core/table', 'stripes');
   wp.blocks.unregisterBlockStyle('core/separator', 'wide');
   wp.blocks.unregisterBlockStyle('core/separator', 'dots');
+  wp.blocks.unregisterBlockStyle('core/social-links', 'logos-only');
+  wp.blocks.unregisterBlockStyle('core/social-links', 'pill-shape');
+  wp.blocks.unregisterBlockStyle('core/social-links', 'outline');
 });
