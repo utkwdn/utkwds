24ce8b662bb10cad964fbe0e906b6f4aab4c6fb7
Mike Henderson <mhenderson@insidenewcity.com>
2024-07-12 13:19:46 -0600

    sidebar pattern

---
+++ src/patterns/secondary-navigation.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Secondary Navigation
+ * Slug: utkwds/secondary-navigation
+ * Description:
+ * Categories: links
+ * Keywords: sidebar, navigation, columns
+ * Viewport Width: 1500 
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:columns {"metadata":{"name":"Sidebar Navigation"},"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|medium"},"margin":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}},"className":"utkwds-sidebar-navigation is-style-columns-reverse"} -->
+<div class="wp-block-columns utkwds-sidebar-navigation is-style-columns-reverse" style="margin-top:var(--wp--preset--spacing--medium);margin-bottom:var(--wp--preset--spacing--medium)"><!-- wp:column {"width":"66.6%","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-column" style="flex-basis:66.6%"><!-- wp:paragraph {"className":"is-style-utkwds-paragraph-large"} -->
+<p class="is-style-utkwds-paragraph-large">Large paragraph or into. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam ex ac libero rhoncus, vel pretium sapien scelerisque. Cras quis commodo elit, sed euismod enim.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Heading Two</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Default paragraph size. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam ex ac libero rhoncus, vel pretium sapien scelerisque. Cras quis commodo elit, sed euismod enim. Vestibulum vitae ex sit amet metus viverra tempor in gravida enim. Nam pretium augue eget nisi ultricies lobortis. Aliquam aliquam, quam dictum condimentum bibendum, lacus lorem efficitur nisi, varius volutpat risus ipsum at ante. Nam elit nisl, sagittis nec dolor in, iaculis ultrices dui. Proin semper pellentesque aliquam.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.3%","layout":{"type":"constrained"}} -->
+<div class="wp-block-column" style="flex-basis:33.3%"><!-- wp:utkwds/secondary-navigation /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
