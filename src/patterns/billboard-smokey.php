a1b49419be3bded09083049dbc866e825ea67b1a
Mike Henderson <mhenderson@insidenewcity.com>
2024-09-05 13:16:40 -0600

    billboard patterns

---
+++ src/patterns/billboard-smokey.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Billboard Smokey
+ * Slug: utkwds/billboard-smokey
+ * Description: 
+ * Categories: banners
+ * Keywords: call to action, change of topic
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Billboard Smokey"},"align":"full","className":"utkwds-billboard","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"smokey","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull utkwds-billboard has-white-color has-smokey-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--x-large)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color"></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"2px","className":"utkwds-billboard-divider","backgroundColor":"orange"} -->
+<div class="wp-block-column utkwds-billboard-divider has-orange-background-color has-background" style="flex-basis:2px"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%","style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-utkwds-cta-link"} -->
+<p class="is-style-utkwds-cta-link"><a href="https://www.utk.edu/">CTA Link</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
