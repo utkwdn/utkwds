4a8f88752acbb4d1528bcb9b36524b51ba758051
Mike Henderson <mhenderson@insidenewcity.com>
2024-09-09 08:19:24 -0600

    billboard pattern starter content

--- src/patterns/billboard-smokey-bg-texture.php
+++ src/patterns/billboard-smokey-bg-texture.php
@@ -17,7 +17,7 @@
 <div class="wp-block-cover alignfull has-white-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--x-large)"><span aria-hidden="true" class="wp-block-cover__background has-smokey-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-131" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/bkg-cot-imagedark.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|medium"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.33%"} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
-<h2 class="wp-block-heading has-white-color has-text-color has-link-color"></h2>
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color">Heading for a Billboard</h2>
 <!-- /wp:heading --></div>
 <!-- /wp:column -->
 
@@ -27,7 +27,7 @@
 
 <!-- wp:column {"width":"66.66%","style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
 <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+<p>A full-width block of text with backgrounds that can be used to break up the layout for a call-to-action, to route users toward another topic, or call attention to an important message. For the heading, use short words and up to 3 lines of text at desktop size.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"className":"is-style-utkwds-cta-link"} -->
