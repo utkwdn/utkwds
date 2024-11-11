66210c137bf41bbd08ca0135634656b1f014721c
Mike Henderson <mhenderson@insidenewcity.com>
2024-11-11 13:57:29 -0700

    update starter text

--- src/patterns/in-page-notice.php
+++ src/patterns/in-page-notice.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Title: In Page Notice
+ * Title: In-page Notice
  * Slug: utkwds/in-page-notice
  * Description: 
  * Categories: banners
@@ -13,14 +13,14 @@
 
 ?>
 
-<!-- wp:group {"metadata":{"name":"In Page Notice","categories":["banners"],"patternName":"utkwds/in-page-notice"},"className":"utkwds-in-page-notice","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}},"border":{"width":"4px"}},"backgroundColor":"light","borderColor":"orange","layout":{"type":"constrained"}} -->
+<!-- wp:group {"metadata":{"name":"In-page Notice","categories":["banners"],"patternName":"utkwds/in-page-notice"},"className":"utkwds-in-page-notice","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}},"border":{"width":"4px"}},"backgroundColor":"light","borderColor":"orange","layout":{"type":"constrained"}} -->
 <div class="wp-block-group utkwds-in-page-notice has-border-color has-orange-border-color has-light-background-color has-background" style="border-width:4px;margin-top:var(--wp--preset--spacing--small);margin-bottom:var(--wp--preset--spacing--small);padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:paragraph -->
-<p><strong>This is the title for an in page notice</strong></p>
+<p><strong>In-page notice</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent <a href="#">per conubia nostra</a>.</p>
+<p>Make a message stand out on a page of details, so deadlines and special instructions don’t get lost in a wall of text. Only use once per page, remove it once its date passes, and aim not to leave up for more than a month, to avoid causing <a href="https://www.nngroup.com/articles/banner-blindness-old-and-new-findings/">banner blindness</a> for your site users.</a>.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
