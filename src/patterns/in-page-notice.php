fd5d0eee690f8cdaa73968527f74ed9c2a7e75e7
Mike Henderson <mhenderson@insidenewcity.com>
2024-11-11 13:29:21 -0700

    in page notice pattern

---
+++ src/patterns/in-page-notice.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: In Page Notice
+ * Slug: utkwds/in-page-notice
+ * Description: 
+ * Categories: banners
+ * Keywords: 
+ * Viewport Width: 1500
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"In Page Notice","categories":["banners"],"patternName":"utkwds/in-page-notice"},"className":"utkwds-in-page-notice","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}},"border":{"width":"4px"}},"backgroundColor":"light","borderColor":"orange","layout":{"type":"constrained"}} -->
+<div class="wp-block-group utkwds-in-page-notice has-border-color has-orange-border-color has-light-background-color has-background" style="border-width:4px;margin-top:var(--wp--preset--spacing--small);margin-bottom:var(--wp--preset--spacing--small);padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong>This is the title for an in page notice</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida dui a aliquet egestas. Class aptent taciti sociosqu ad litora torquent <a href="#">per conubia nostra</a>.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
