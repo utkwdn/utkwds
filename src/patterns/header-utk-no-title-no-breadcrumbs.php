16f58619a39b4d60aaafac98145f8a36ae0d4953
Mike Henderson <mhenderson@insidenewcity.com>
2024-12-04 10:48:58 -0700

    site editor header fix

--- src/patterns/header-utk-no-title-no-breadcrumbs.php
+++ src/patterns/header-utk-no-title-no-breadcrumbs.php
@@ -14,13 +14,14 @@
 <!-- wp:group {"className":"wp-block-group header-site-title-wrapper universal-header__inner-blocks","layout":{"type":"default"}} -->
 <div class="wp-block-group header-site-title-wrapper universal-header__inner-blocks">
 <?php if ( get_theme_mod( 'parent_label' ) ) : ?>
-    <p class="header-site-tagline"> 
+    <!-- wp:paragraph {"className":"header-site-tagline"} -->
+    <p class="header-site-tagline">
       <?php if ( get_theme_mod( 'parent_link' ) ) : ?>
         <a href="<?php echo esc_url( get_theme_mod( 'parent_link' ) ); ?>">
-          <?php echo esc_html(get_theme_mod( 'parent_label' )); ?></p>
-        </a>
+          <?php echo esc_html(get_theme_mod( 'parent_label' )); ?>
+        </a></p><!-- /wp:paragraph -->
       <?php else : ?>
-        <?php echo esc_html(get_theme_mod( 'parent_label' )); ?></p>
+        <?php echo esc_html(get_theme_mod( 'parent_label' )); ?></p><!-- /wp:paragraph -->
       <?php endif; ?>
   <?php endif; ?>
   <!-- wp:site-title {"level":0,"isLink":true,"style":{"typography":{"lineHeight":"1.2"}},"className": "header-site-title"} /--></div>
