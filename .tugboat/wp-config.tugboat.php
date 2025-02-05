cfd751f9ed84f056dbcb3a726e50e1eadb29af82
Mike Henderson <mhenderson@insidenewcity.com>
2025-02-05 11:02:52 -0700

    revert changes

--- .tugboat/wp-config.tugboat.php
+++ .tugboat/wp-config.tugboat.php
@@ -12,6 +12,33 @@ define( 'WP_HOME', 'https://' . getenv('TUGBOAT_SERVICE_URL_HOST') );
 // Define the location where WordPress Core is installed.
 // In this case, a subdirectory of the webroot called "wp".
 define( 'WP_SITEURL', 'https://' . getenv('TUGBOAT_SERVICE_URL_HOST') . '/' );
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+// ** Authentication Unique Keys and Salts. ** //
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/** WordPress Database Table prefix. */
+$table_prefix = 'wp_';
 
 /** For developers: WordPress debugging mode. */
 define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
