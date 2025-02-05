b04548ed22d5e4159184c2db3230f04e10c08d9a
Phil Pelanne <phil@insidenewcity.com>
2025-02-05 17:35:55 -0500

    percy.js adjustments to parse json

--- src/tests/percy/percy.js
+++ src/tests/percy/percy.js
@@ -11,7 +11,7 @@ import percySnapshot from '@percy/puppeteer';
 
 (async () => {
   // Create a new browser instance
-  const browser = await puppeteer.launch({ headless: true,'--no-sandbox', '--disable-setuid-sandbox' });
+  const browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.setViewport({ width: 1080, height: 1024 });
 
