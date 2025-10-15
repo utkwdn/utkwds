da1dd36223f43c961ddf499e65e70460011b276a
Mark Naylor <marknaylor@MacBookPro.attlocal.net>
2025-10-15 15:04:53 -0500

    feat(functions): add filter to disable admin email verification

--- src/functions.php
+++ src/functions.php
@@ -41,22 +41,21 @@ if ( ! function_exists( 'utkwds_setup' ) ) {
 
 		// Remove core block patterns.
 		remove_theme_support( 'core-block-patterns' );
-
 	}
 }
 add_action( 'after_setup_theme', 'utkwds_setup' );
 
 // Enqueue style sheet.
 function utkwds_enqueue_style_sheet() {
-	
+
 	// asset file is built by webpack
 	$asset = include get_parent_theme_file_path( '/screen.asset.php' );
-	
-	wp_enqueue_style( 
-		'utkwds', 
-		get_template_directory_uri() . '/screen.css', 
+
+	wp_enqueue_style(
+		'utkwds',
+		get_template_directory_uri() . '/screen.css',
 		$asset['dependencies'],
-		$asset['version'] 
+		$asset['version']
 	);
 }
 add_action( 'wp_enqueue_scripts', 'utkwds_enqueue_style_sheet' );
@@ -81,32 +80,31 @@ add_action( 'wp_enqueue_scripts', 'utk_offcanvas_script' );
 
 function utkwds_editor_assets() {
 
-  $asset = include get_parent_theme_file_path( '/js/block-variations.asset.php' );
+	$asset = include get_parent_theme_file_path( '/js/block-variations.asset.php' );
 
 	wp_enqueue_script(
 		'utkwds-block-variations',
 		get_parent_theme_file_uri() . '/js/block-variations.js',
-    $asset['dependencies'],
-    $asset['version'],
+		$asset['dependencies'],
+		$asset['version'],
 		false
 	);
-
 }
 add_action( 'enqueue_block_editor_assets', 'utkwds_editor_assets' );
 
-require_once( 'inc/functions/block-styles.php');
-require_once( 'inc/functions/customizer.php');
-require_once( 'inc/functions/editor-restrictions.php');
-require_once( 'inc/functions/footer-widget.php');
-require_once( 'inc/functions/google-tag-manager.php');
-require_once( 'inc/functions/inc-menu.php');
-require_once( 'inc/functions/inc-patterns.php');
-require_once( 'inc/functions/inc-search.php');
-require_once( 'inc/functions/kses.php');
-require_once( 'inc/functions/render-block.php');
-require_once( 'inc/functions/shortcodes.php');
-require_once( 'inc/functions/theme-update.php');
-require_once( 'inc/functions/user-roles.php');
+require_once 'inc/functions/block-styles.php';
+require_once 'inc/functions/customizer.php';
+require_once 'inc/functions/editor-restrictions.php';
+require_once 'inc/functions/footer-widget.php';
+require_once 'inc/functions/google-tag-manager.php';
+require_once 'inc/functions/inc-menu.php';
+require_once 'inc/functions/inc-patterns.php';
+require_once 'inc/functions/inc-search.php';
+require_once 'inc/functions/kses.php';
+require_once 'inc/functions/render-block.php';
+require_once 'inc/functions/shortcodes.php';
+require_once 'inc/functions/theme-update.php';
+require_once 'inc/functions/user-roles.php';
 
 // Register our custom blocks in the theme
 function utkwds_block_init() {
@@ -114,42 +112,44 @@ function utkwds_block_init() {
 	// asset file is built by webpack
 	$accordion_script = include get_parent_theme_file_path( 'blocks/accordion/accordion.asset.php' );
 
-	wp_register_script( 
-		'utk-wds-accordion', 
-		get_parent_theme_file_uri() . 'blocks/accordion/accordion.js', 
-		$accordion_script['dependencies'], 
-		$accordion_script['version'], 
+	wp_register_script(
+		'utk-wds-accordion',
+		get_parent_theme_file_uri() . 'blocks/accordion/accordion.js',
+		$accordion_script['dependencies'],
+		$accordion_script['version'],
 		true
 	);
-	
+
 	register_block_type( __DIR__ . '/blocks/accordion' );
 	register_block_type( __DIR__ . '/blocks/accordion-panel' );
 	register_block_type( __DIR__ . '/blocks/breadcrumbs' );
-  register_block_type( __DIR__ . '/blocks/icon' );
+	register_block_type( __DIR__ . '/blocks/icon' );
 	register_block_type( __DIR__ . '/blocks/nav-menu' );
-  register_block_type( __DIR__ . '/blocks/secondary-navigation' );
+	register_block_type( __DIR__ . '/blocks/secondary-navigation' );
 	register_block_type( __DIR__ . '/blocks/site-header' );
 	register_block_type( __DIR__ . '/blocks/site-footer' );
 	register_block_type( __DIR__ . '/blocks/tab-group' );
 	register_block_type( __DIR__ . '/blocks/tab' );
-
 }
 
-add_action( 'init', 'utkwds_block_init');
+add_action( 'init', 'utkwds_block_init' );
 
-add_filter( 'block_type_metadata', function ( $metadata ) {
-	return $metadata;
-});
+add_filter(
+	'block_type_metadata',
+	function ( $metadata ) {
+		return $metadata;
+	}
+);
 
 function utkwds_register_vars() {
 	wp_register_script( 'utk-wds-navigation-blocks-vars', false );
 	wp_enqueue_script( 'utk-wds-navigation-blocks-vars' );
-	
+
 	wp_add_inline_script(
 		'utk-wds-navigation-blocks-vars',
 		'const UTKWDS = ' . json_encode(
 			array(
-				'theme_url' => get_template_directory_uri(),
+				'theme_url'   => get_template_directory_uri(),
 				'blocks_path' => '/build/blocks/',
 			)
 		),
@@ -160,23 +160,26 @@ add_action( 'wp_enqueue_scripts', 'utkwds_register_vars' );
 add_action( 'admin_enqueue_scripts', 'utkwds_register_vars' );
 
 function utkwds_editor_restrict_css() {
-  
-  if ( current_user_can( 'edit_theme_options' ) ) {
-    return;
-  }
 
-  $asset = include get_parent_theme_file_path( '/editor-restrict.asset.php');
-  
-  wp_enqueue_style(
-    'utkwds-editor-restrict',
-    get_template_directory_uri() . '/editor-restrict.css',
-    $asset['dependencies'],
-		$asset['version'] 
-  );
+	if ( current_user_can( 'edit_theme_options' ) ) {
+		return;
+	}
+
+	$asset = include get_parent_theme_file_path( '/editor-restrict.asset.php' );
+
+	wp_enqueue_style(
+		'utkwds-editor-restrict',
+		get_template_directory_uri() . '/editor-restrict.css',
+		$asset['dependencies'],
+		$asset['version']
+	);
 }
 
 add_action( 'enqueue_block_editor_assets', 'utkwds_editor_restrict_css' );
 
-if (defined('WP_CLI') && WP_CLI) {
-  require_once( 'tests/kitchensink.php' );
+// Disable WordPress Administration Email verification Screen.
+add_filter( 'admin_email_check_interval', '__return_false' );
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	require_once 'tests/kitchensink.php';
 }
