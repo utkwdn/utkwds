c3625a8bcb34befddf58618a4be76aae5674dd00
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2026-01-12 09:31:00 -0600

    refactor(search): update search template part

--- src/classic-template-parts/gcse-search.php
+++ src/classic-template-parts/gcse-search.php
@@ -7,10 +7,28 @@
  * @package utkwds
  */
 
+
+// Read query from URL (?s=term).
+// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+$search_query = isset( $_GET['s'] ) ? sanitize_text_field( wp_unslash( $_GET['s'] ) ) : '';
 ?>
 
-<!-- Google SCE script is enqueued by utk_wds_google_search(), located
-	in `inc/functions/inc-search.php` (which is required in functions.php) -->
-	
-<div class="gcse-searchbox" data-gname="utk-global-search" data-queryParameterName="s"></div>
-<div class="gcse-searchresults" data-gname="utk-global-search" data-enableImageSearch="false" data-enableOrderBy="false" data-queryParameterName="s"></div>
+<div class="utk-search-wrapper" style="margin-bottom: 20px;">
+
+	<form class="utk-search-form" method="get" action="">
+		<label for="search-input" class="screen-reader-text">Search</label>
+		<input 
+			type="text" 
+			id="search-input" 
+			name="s" 
+			placeholder="Search utk.edu…" 
+			value="<?php echo esc_attr( $search_query ); ?>"
+		/>
+		<button type="submit">Search</button>
+	</form>
+
+	<div id="utk-search-results" style="margin-top:20px;"></div>
+
+	<div id="utk-search-pagination" class="pagination"></div>
+
+</div>
