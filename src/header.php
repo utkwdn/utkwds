84ec50450842e7ef72ed1a5f1c269cf305ad8b3c
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2026-02-05 14:00:09 -0600

    refactor(header): add meta viewport tag to header

--- src/header.php
+++ src/header.php
@@ -11,6 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
