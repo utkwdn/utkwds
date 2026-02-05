29fe93be543c233ec289dbe7a1c720284990e6bc
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2026-02-05 10:11:39 -0600

    chore(search): remove commented out code

--- src/search.php
+++ src/search.php
@@ -34,19 +34,6 @@ get_header();
 <div class="nav nav-tabs tab-content main-tabs-content search-tab" id="myTabContent">
 	<div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab">
 	<h2 class="utk-global-search-heading">Search results from: <?php bloginfo( 'name' ); ?></h2>
-	<!-- <form class="form-inline hidden-print mt-4" id="site-search-form" action="<?php bloginfo( 'wpurl' ); ?>/">
-		<div class="mb-3 input-group">
-		<label class="sr-only visually-hidden" for="site-search-input-tabpanel">Search It</label>
-		<input type="search" class="form-control" title="Search this site" placeholder="Search"  name="s" id="site-search-input-tabpanel" />
-		<button type="submit" class="btn btn-utlink">
-		<svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<title>Search Icon</title>
-				<circle cx="6.12" cy="5.73" r="4.22" transform="matrix(0.99999, 0.00372, -0.00372, 0.99999, 0.02135, -0.02272)" stroke-width="2"></circle>
-				<line x1="9.35" y1="8.41" x2="12.71" y2="11.8" stroke-width="2"></line>
-			</svg>
-		<span>Search</span></button>
-		</div>
-	</form> -->
 	<form class="utk-site-search-form" method="get" action="">
 		<div class="form-floating">
 			<input
