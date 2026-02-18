74b66786daf5b43976060f291fa23956caf9803d
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2026-02-18 09:34:46 -0600

    refactor(theme-update): update to use package.json version number instead of info.json

--- src/inc/functions/theme-update.php
+++ src/inc/functions/theme-update.php
@@ -21,10 +21,15 @@ function utkwds_update_theme( $transient ) {
 	$theme   = wp_get_theme();
 	$version = $theme->get( 'Version' );
 
-	if ( false == $remote = get_transient( 'utkwds_update_theme' . $version ) ) {
+	$github_org  = 'utkwdn';
+	$github_repo = 'utkwds';
+
+	$remote = get_transient( 'utkwds_update_theme' . $version );
+
+	if ( false === $remote ) {
 
 		$remote = wp_remote_get(
-			'https://raw.githubusercontent.com/utkwdn/utkwds/main/info.json',
+			"https://raw.githubusercontent.com/{$github_org}/{$github_repo}/main/package.json",
 			array(
 				'timeout' => 10,
 				'headers' => array(
@@ -34,29 +39,30 @@ function utkwds_update_theme( $transient ) {
 		);
 
 		if ( is_wp_error( $remote )
-		|| 200 !== wp_remote_retrieve_response_code( $remote )
-		|| empty( wp_remote_retrieve_body( $remote ) )
-		) {
+			|| 200 !== wp_remote_retrieve_response_code( $remote )
+			|| empty( wp_remote_retrieve_body( $remote ) )
+			) {
 			return $transient;
 		}
-
-		// Encode the response body.
-		$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+			// Encode the response body.
+			$remote = json_decode( wp_remote_retrieve_body( $remote ) );
 
 		if ( ! $remote ) {
 			return $transient; // who knows, maybe JSON is not valid.
 		}
-
-		// Set the response transient for theme performance, 12 hours.
-		set_transient( 'utkwds_update_theme' . $version, $remote, 43200 );
+			// Set the response transient for theme performance, 12 hours.
+			set_transient( 'utkwds_update_theme' . $version, $remote, 43200 );
 
 	}
 
+	$details_url  = "https://github.com/{$github_org}/{$github_repo}/releases/tag/v{$remote->version}";
+	$download_url = "https://github.com/{$github_org}/{$github_repo}/releases/download/v{$remote->version}/utkwds.zip";
+
 	$data = array(
 		'theme'       => $stylesheet,
-		'url'         => $remote->details_url,
+		'url'         => $details_url,
 		'new_version' => $remote->version,
-		'package'     => $remote->download_url,
+		'package'     => $download_url,
 	);
 
 	if ( ! is_array( $transient->response ) ) {
@@ -64,10 +70,8 @@ function utkwds_update_theme( $transient ) {
 	}
 
 	if ( $remote && version_compare( $version, $remote->version, '<' ) ) {
-
 		$transient->response[ $stylesheet ] = $data;
 	} else {
-
 		$transient->no_update[ $stylesheet ] = $data;
 	}
 
