6dfc703bb03cca57943afd5b5620b7e0d3d214b8
Corbin Schappel <cschappe@utk.edu>
2026-02-27 15:24:12 -0500

    feat(hero-split): initial work on new hero split

---
+++ src/patterns/hero-split.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Hero Split
+ * Slug: utkwds/hero-split
+ * Description: A split media and content pattern—with space to fill the top header area of a high-level or landing page.
+ * Categories: hero
+ * Keywords: full-width, full width, header, hero, image, large image, single image, link, CTA, CTA link, cover, title
+ * Viewport Width: 1500
+ * Inserter: true
+ *
+ * @package utkwds
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Hero Split"},"align":"full","className":"utkwds-hero-split"} -->
+<div class="wp-block-group utkwds-hero-split alignfull"><!-- wp:media-text {"align":"wide","mediaLink":"<?php echo esc_html( get_stylesheet_directory_uri() ); ?>/assets/images/repeat-placeholder-1700x700.jpg","mediaType":"image","verticalAlignment":"center"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center"><figure class="wp-block-media-text__media"><img src="<?php echo esc_html( get_stylesheet_directory_uri() ); ?>/assets/images/repeat-placeholder-1700x700.jpg" /></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+<h2 class="wp-block-heading">test</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>fdsafdsafdsaf</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-utkwds-cta-link"} -->
+<p class="is-style-utkwds-cta-link"><a href="https://www.utk.edu/">fdsfdsfdf</a></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text --></div>
+<!-- /wp:group -->
