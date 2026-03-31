78e55bb6e08d9eafb5857b2bd4aa1decf2159c26
Mark Naylor <mnaylor@utk.edu>
2026-03-31 15:23:21 -0500

    refactor(pagination): apply search pagination styles to news

--- src/patterns/query-archive.php
+++ src/patterns/query-archive.php
@@ -47,10 +47,8 @@
 <!-- /wp:separator -->
 
 <!-- wp:query-pagination -->
-<!-- wp:query-pagination-previous /-->
 
 <!-- wp:query-pagination-numbers /-->
 
-<!-- wp:query-pagination-next /-->
 <!-- /wp:query-pagination --></div>
 <!-- /wp:query -->
