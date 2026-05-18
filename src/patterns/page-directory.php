0278af29c815cd22dd9db01a45ab00e8ba9a0158
Corbin Schappel <cschappe@utk.edu>
2026-05-18 18:22:16 -0400

    feat(directory): init new Directory template

---
+++ src/patterns/page-directory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Directory
+ * Slug: utkwds/page-directory
+ * Categories: page-layouts
+ * Block Types: core/post-content
+ * Post Types: page
+ *
+ * @package utkwds
+ */
+
+?>
+
+<!-- wp:paragraph -->
+<p>Duis in lacus a tortor pharetra euismod eget et nisi. Cras imperdiet sollicitudin euismod. Vivamus eget maximus ante. Donec ullamcorper ultricies turpis, sit amet tempus augue faucibus nec. Aenean quis leo vehicula magna mattis auctor. Aenean eu hendrerit magna, vel placerat arcu. Proin faucibus laoreet nisi non accumsan. Etiam sit amet bibendum ipsum. Aliquam at sem vel elit facilisis bibendum non sit amet justo. In iaculis egestas accumsan.</p>
+<!-- /wp:paragraph -->
