9df5e0f8b9d8805c5329a906bdb6eea4ba33cc65
Corbin Schappel <cschappe@utk.edu>
2026-07-20 12:44:31 -0400

    feat(cta-category-listing): refactor to grid for easy change on mobile layout

--- src/patterns/post-horizontal.php
+++ src/patterns/post-horizontal.php
@@ -14,16 +14,12 @@
 
 ?>
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:post-title {"isLink":true,"className":"heading-style\u002d\u002dh6"} /-->
+<!-- wp:group {"metadata":{"patternName":"utkwds/post-horizontal","name":"Post Horizontal"},"layout":{"type":"grid"}} -->
+<div class="wp-block-group"><!-- wp:post-featured-image {"aspectRatio":"3/2"} /-->
+
+<!-- wp:post-title {"isLink":true,"className":"heading-style\u002d\u002dh6"} /-->
 
 <!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"fontSize":"x-small"} /-->
 
 <!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":20} /--></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:post-featured-image {"aspectRatio":"3/2","width":"220px"} /--></div>
-<!-- /wp:group --></div>
 <!-- /wp:group -->
