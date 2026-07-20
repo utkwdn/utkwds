380fd4b4eea183adccb17730d307fd515c1dc43f
Corbin Schappel <cschappe@utk.edu>
2026-07-20 18:11:37 -0400

    feat(cta-category-listing): change post title to h3, image position

--- src/patterns/post-horizontal.php
+++ src/patterns/post-horizontal.php
@@ -17,7 +17,7 @@
 <!-- wp:group {"metadata":{"patternName":"utkwds/post-horizontal","name":"Post Horizontal"},"layout":{"type":"grid"}} -->
 <div class="wp-block-group"><!-- wp:post-featured-image {"aspectRatio":"3/2"} /-->
 
-<!-- wp:post-title {"isLink":true,"className":"heading-style\u002d\u002dh6"} /-->
+<!-- wp:post-title {"level":3,"isLink":true,"className":"heading-style--h6"} /-->
 
 <!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"fontSize":"x-small"} /-->
 
