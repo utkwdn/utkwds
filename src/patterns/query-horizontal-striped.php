671cbc3eb287379ccf510e2adbc634abe0e9b433
Corbin Schappel <cschappe@utk.edu>
2026-08-05 11:57:51 -0400

    feat(archive-template): refactor archive template, rename query pattern to more reusable name

---
+++ src/patterns/query-horizontal-striped.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: List of posts in horizontal rows with alternating backgrounds and an orange top border, respecting the category/year filters.
+ * Slug: utkwds/query-horizontal-striped
+ * Inserter: false
+ *
+ * @package utkwds
+ */
+
+?>
+
+<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":10},"className":"utkwds-query-horizontal-striped","layout":{"type":"constrained"}} -->
+<div class="wp-block-query utkwds-query-horizontal-striped"><!-- wp:post-template -->
+
+<!-- wp:pattern {"slug":"utkwds/post-horizontal"} /-->
+
+<!-- /wp:post-template -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph -->
+<p>No posts match the selected filters.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results -->
+
+<!-- wp:query-pagination -->
+	<!-- wp:query-pagination-numbers {"midSize": 1} /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:query -->
