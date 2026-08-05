99a8b97a5889c5d7739b715707dc059e94a217c4
Corbin Schappel <cschappe@utk.edu>
2026-08-05 12:21:09 -0400

    feat(archive-template): rename query pattern to be more descriptive

--- src/patterns/query-post-horizontal-striped.php
+++ src/patterns/query-post-horizontal-striped.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: List of posts in horizontal rows with alternating backgrounds and an orange top border, respecting the category/year filters.
- * Slug: utkwds/query-horizontal-striped
+ * Slug: utkwds/query-post-horizontal-striped
  * Inserter: false
  *
  * @package utkwds
@@ -9,8 +9,8 @@
 
 ?>
 
-<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":10},"className":"utkwds-query-horizontal-striped","layout":{"type":"constrained"}} -->
-<div class="wp-block-query utkwds-query-horizontal-striped"><!-- wp:post-template -->
+<!-- wp:query {"queryId":0,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"perPage":10},"className":"utkwds-query-post-horizontal-striped","layout":{"type":"constrained"}} -->
+<div class="wp-block-query utkwds-query-post-horizontal-striped"><!-- wp:post-template -->
 
 <!-- wp:pattern {"slug":"utkwds/post-horizontal"} /-->
 
