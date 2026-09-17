da683f03a500e8d2c62dc863f086ff84d240422a
Mark Naylor <mnaylor@utk.edu>
2026-09-17 13:03:09 -0500

    feat(patterns): add itav page pattern

---
+++ src/patterns/page-itav.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: It Takes a Volunteer Page
+ * Slug: utkwds/page-itav
+ * Categories: page-layouts
+ * Block Types: core/post-content
+ * Post Types: page
+ *
+ * @package utkwds
+ */
+
+?>
+
+<!-- wp:pattern {"slug":"utkwds/hero-split"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/media-text-cta-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/callout-orange"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/media-text-cta-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/text-cta-media-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/media-text-cta-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/points-of-pride-smokey"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/quote-media-orange-white"} /-->
+
+<!-- wp:pattern {"slug":"utkwds/stack-3up-light-gray"} /-->
